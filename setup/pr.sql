3a0f570096ddbea51c0887fa66b1a09a75858402
Fogel <fogelstrande@gmail.com>
2025-05-29 23:03:13 +0200

    databas o python kan prata nu :O

--- setup/pr.sql
+++ setup/pr.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS Groups (
-    name TEXT PRIMARY KEY
+    name TEXT PRIMARY KEY CHECK (LENGTH(name) >= 3)
 );
 
 CREATE TABLE IF NOT EXISTS Posts (
@@ -8,15 +8,18 @@ CREATE TABLE IF NOT EXISTS Posts (
     file_name TEXT NOT NULL,
     start_time TIMESTAMP NOT NULL,
     end_time TIMESTAMP NOT NULL,
-    owner TEXT REFERENCES Groups(name)
+    owner TEXT NOT NULL REFERENCES Groups(name) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS PostViews (
-    route TEXT PRIMARY KEY CHECK (route ~ '^[a-z0-9_-]+$'), -- case sensative, only allow lower-cased letters, numbers, underscores, and dashes
-    owner TEXT REFERENCES Groups(name)
+    id SERIAL PRIMARY KEY,
+    route TEXT NOT NULL UNIQUE CHECK (route ~ '^[a-z0-9_-]+$'), -- only lowercase letters, numbers, underscores, and dashes
+    name TEXT NOT NULL,
+    owner TEXT NOT NULL REFERENCES Groups(name) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS PostViewContents (
-    view_route TEXT REFERENCES PostViews(route) ON DELETE CASCADE,
-    post_id INT REFERENCES Posts(id) ON DELETE CASCADE  
+    view_id INT NOT NULL REFERENCES PostViews(id) ON DELETE CASCADE,
+    post_id INT NOT NULL REFERENCES Posts(id) ON DELETE CASCADE,
+    PRIMARY KEY (view_id, post_id) 
 );
--- src/database/pr.py
+++ src/database/pr.py
@@ -18,19 +18,97 @@ def create_post(
     end_time_parsed: datetime = parse(end_time)
 
     cur.execute(
-        "INSERT INTO Posts (description, file_name, start_time, end_time, owner) VALUES (%s, %s, %s, %s, admin)",
-        (description, file_name, start_time_parsed, end_time_parsed),
+        "INSERT INTO Posts (description, file_name, start_time, end_time, owner) VALUES (%s, %s, %s, %s, %s);",
+        (description, file_name, start_time_parsed, end_time_parsed, "admin"),
     )
 
 
 @pr_cursor
-def create_postview(cur: cursor, route: str):
-    cur.execute("INSERT INTO PostViews (route, owner) VALUES (%s, admin)", (route,))
+def delete_post(cur: cursor, id: int):
+    cur.execute("DELETE FROM Posts WHERE id=%s;", (id,))
 
 
 @pr_cursor
-def add_post_to_postview(cur: cursor, view_route: str, post_id: int):
+def change_post_description(cur: cursor, id: int, new_description: str):
+    cur.execute("UPDATE Posts SET description=%s WHERE id=%s;", (new_description, id))
+
+
+@pr_cursor
+def change_post_times(cur: cursor, id: int, new_start_time: str, new_end_time: str):
+    new_start_time_parsed: datetime = parse(new_start_time)
+    new_end_time_parsed: datetime = parse(new_end_time)
+
     cur.execute(
-        "INSERT INTO PostViewContents (view_route, post_id) VALUES (%s, %s)",
-        (view_route, post_id),
+        "UPDATE Posts SET start_time=%s, end_time=%s WHERE id=%s;",
+        (new_start_time_parsed, new_end_time_parsed, id),
     )
+
+
+@pr_cursor
+def get_post(cur: cursor, id: int) -> tuple[str, str, datetime, datetime, str]:
+    cur.execute(
+        "SELECT description, file_name, start_time, end_time, owner FROM Posts WHERE id=%s;",
+        (id,),
+    )
+    row = cur.fetchone()
+    if row is None:
+        raise ValueError(f"No post found with id {id}")
+
+    post: tuple[str, str, datetime, datetime, str] = row
+    return post
+
+
+@pr_cursor
+def create_postview(cur: cursor, route: str, name: str):
+    cur.execute(
+        "INSERT INTO PostViews (route, name, owner) VALUES (%s, %s, %s);",
+        (route, name, "admin"),
+    )
+
+
+@pr_cursor
+def delete_postview(cur: cursor, id: int):
+    cur.execute("DELETE FROM PostViews WHERE id=%s;", (id,))
+
+
+@pr_cursor
+def change_postview_name(cur: cursor, id: int, new_name: str):
+    cur.execute("UPDATE PostViews SET name=%s WHERE id=%s;", (new_name, id))
+
+
+@pr_cursor
+def change_postview_route(cur: cursor, id: int, new_route: str):
+    cur.execute("UPDATE PostViews SET route=%s WHERE id=%s;", (new_route, id))
+
+
+@pr_cursor
+def get_groups_postviews(cur: cursor, group: str) -> list[tuple[int, str, str]]:
+    cur.execute("SELECT id, route, name FROM PostViews WHERE owner=%s;", (group,))
+
+    postviews: list[tuple[int, str, str]] = cur.fetchall()
+    return postviews
+
+
+@pr_cursor
+def add_post_to_postview(cur: cursor, view_id: int, post_id: int):
+    cur.execute(
+        "INSERT INTO PostViewContents (view_id, post_id) VALUES (%s, %s);",
+        (view_id, post_id),
+    )
+
+
+@pr_cursor
+def remove_post_from_postview(cur: cursor, view_id: int, post_id: int):
+    cur.execute(
+        "DELETE FROM PostViewContents WHERE view_id=%s AND post_id=%s;",
+        (view_id, post_id),
+    )
+
+
+@pr_cursor
+def get_content_from_postview(cur: cursor, view_id: int) -> list[int]:
+    cur.execute("SELECT post_id FROM PostViewContents WHERE view_id=%s;", (view_id,))
+    rows: list[tuple[int]] = cur.fetchall()
+    posts: list[int] = [row[0] for row in rows]
+
+    return posts
